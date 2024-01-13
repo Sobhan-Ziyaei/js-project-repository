779450a790401c4c83a8023109bc50056ff962f5
Sobhan <ziaey@gmail.com>
2024-01-13 12:45:42 +0330

    change btn done

--- project_8/js/script.js
+++ project_8/js/script.js
@@ -1,5 +1,6 @@
 const cSpanTitle = document.querySelector(".C");
 const fSpanTitle = document.querySelector(".F");
+const mainInput = document.getElementById("converter");
 const convertBtn = document.querySelector(".convertButton");
 const resetBtn = document.querySelector(".resetButton");
 const changeBtn = document.querySelector(".changeButton");
@@ -11,6 +12,14 @@ const changeBtnClick = () => {
   fSpanTitle.innerHTML === "°F"
     ? (fSpanTitle.innerHTML = "°C")
     : (fSpanTitle.innerHTML = "°F");
+
+  mainInput.getAttribute("placeholder") === "°F"
+    ? mainInput.setAttribute("placeholder", "°C")
+    : mainInput.setAttribute("placeholder", "°F");
+
+  document.title === "Project 8 °C to °F"
+    ? (document.title = "Project 8 °F to °C")
+    : (document.title = "Project 8 °C to °F");
 };
 
 changeBtn.addEventListener("click", changeBtnClick);

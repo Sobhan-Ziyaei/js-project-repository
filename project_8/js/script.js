ddebc2a295838b23a45769fbd2769712fe09eb89
Sobhan <ziaey@gmail.com>
2024-01-14 18:28:19 +0330

    complete project 8

--- project_8/js/script.js
+++ project_8/js/script.js
@@ -1,5 +1,6 @@
 const cSpanTitle = document.querySelector(".C");
 const fSpanTitle = document.querySelector(".F");
+const resultText = document.querySelector(".result");
 const mainInput = document.getElementById("converter");
 const convertBtn = document.querySelector(".convertButton");
 const resetBtn = document.querySelector(".resetButton");
@@ -22,4 +23,31 @@ const changeBtnClick = () => {
     : (document.title = "Project 8 °C to °F");
 };
 
+const convertBtnClick = () => {
+  let valueInput;
+
+  if (mainInput.value === "") {
+    resultText.innerHTML = "Please Enter the value";
+  } else {
+    valueInput = mainInput.value;
+    if (mainInput.getAttribute("placeholder") === "°C") {
+      fDegreeResult = (valueInput * 9) / 5 + 32;
+      resultText.innerHTML = `The fahrenheit degree is ${fDegreeResult}`;
+    } else {
+      cDegreeResult = ((valueInput - 32) * 5) / 9;
+      resultText.innerHTML = `The centigrade degree is ${cDegreeResult}`;
+    }
+  }
+};
+
+const resetBtnClick = () => {
+  resultText.innerHTML = "";
+
+  mainInput.value === ""
+    ? (resultText.innerHTML = "Do not input empty value")
+    : (mainInput.value = "");
+};
+
+resetBtn.addEventListener("click", resetBtnClick);
+convertBtn.addEventListener("click", convertBtnClick);
 changeBtn.addEventListener("click", changeBtnClick);

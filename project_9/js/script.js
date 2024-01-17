2845966c294df3ead5d0a8ddd81ff6f7295fb765
sobhan ziyaei <sobhan.ziaee73@gmail.com>
2024-01-17 20:49:41 +0330

    complete project 9

--- project_9/js/script.js
+++ project_9/js/script.js
@@ -1,11 +1,23 @@
-let locationKey = document.querySelector("#location");
-let title = document.querySelector("title");
+const locationKey = document.querySelector("#location");
+const title = document.querySelector("title");
+const keyElement = document.getElementById("key");
+const locationElement = document.getElementById("location");
+const whichElement = document.getElementById("which");
+const codeElement = document.getElementById("code");
+const keyCodeElement = document.getElementById("keyCode");
 
 const startProject = (event) => {
+  console.log(event);
+  event.preventDefault();
   starter.style.display = "none";
   heading.style.display = "flex";
   ascii.style.display = "flex";
   infos.style.display = "flex";
+  keyElement.innerHTML = event.key;
+  locationElement.innerHTML = event.location;
+  whichElement.innerHTML = event.which;
+  codeElement.innerHTML = event.code;
+  keyCodeElement.innerHTML = event.keyCode;
 };
 
 document.body.addEventListener("keydown", startProject);

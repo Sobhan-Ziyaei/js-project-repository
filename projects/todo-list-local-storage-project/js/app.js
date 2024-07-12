c04b3a3dffeaaef87fd651794e0daf49a1b111d1
sobhan <sobhan.ziaee73@gmail.com>
2024-07-12 22:03:06 +0330

    start todo list local storage project

---
+++ projects/todo-list-local-storage-project/js/app.js
@@ -0,0 +1,82 @@
+
+let inputElem = document.querySelector('input');
+let addBtn = document.querySelector('#addButton');
+let clearAllBtn = document.querySelector('#clearButton');
+let ulElem = document.querySelector('#todoList');
+
+let todosList = [];
+
+addTodo = () => {
+    let inputValue = inputElem.value;
+
+    let newTodoObj = {
+        id: todosList.length + 1,
+        title: inputValue,
+        status: false,
+    }
+
+    inputElem.value = '';
+
+    todosList.push(newTodoObj);
+    setLocalStorage(todosList);
+    todosGenerator(todosList);
+
+    inputElem.focus();
+};
+
+setLocalStorage = (todosList) => {
+    localStorage.setItem('todos', JSON.stringify(todosList));
+}
+
+todosGenerator = (todosList) => {
+
+    let newTodoLiElem, newTodoLabelElem, newTodoCompleteBtn, newTodoDeleteBtn;
+
+    ulElem.innerHTML = '';
+
+    todosList.forEach(todo => {
+        newTodoLiElem = document.createElement('li');
+        newTodoLiElem.className = 'completed well';
+
+        newTodoLabelElem = document.createElement('label');
+        newTodoLabelElem.innerHTML = todo.title;
+
+        newTodoCompleteBtn = document.createElement('button');
+        newTodoCompleteBtn.className = 'btn btn-success';
+        newTodoCompleteBtn.innerHTML = 'Complete';
+
+        newTodoDeleteBtn = document.createElement('button');
+        newTodoDeleteBtn.className = 'btn btn-danger';
+        newTodoDeleteBtn.innerHTML = 'Delete';
+
+        newTodoLiElem.append(newTodoLabelElem, newTodoCompleteBtn, newTodoDeleteBtn);
+
+        ulElem.append(newTodoLiElem);
+
+    });
+};
+
+getTodoList = () => {
+    let todos = localStorage.getItem('todos');
+    if (todos) {
+        todosList = JSON.parse(todos);
+    } else {
+        todosList = [];
+    }
+    todosGenerator(todosList);
+}
+
+clearAllTodos = () => {
+    todosList = [];
+    todosGenerator(todosList);
+    localStorage.removeItem('todos');
+}
+
+window.addEventListener('load', getTodoList);
+addBtn.addEventListener('click', addTodo);
+clearAllBtn.addEventListener('click', clearAllTodos);
+inputElem.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+        addTodo();
+    }
+})
